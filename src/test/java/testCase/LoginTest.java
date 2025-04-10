cd0c0c2d5af28be76bb11b37f70c416841fe1234
Sumit.p <sumit.p@peppefry.com>
2025-04-10 11:41:52 +0530

    Fixed cart and Payment issues with the locators

--- src/test/java/testCase/LoginTest.java
+++ src/test/java/testCase/LoginTest.java
@@ -109,7 +109,7 @@ public class LoginTest extends BaseTest {
 
 	private void handlePopupIfPresent() {
 		try {
-			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 			wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(
 					By.id("webklipper-publisher-widget-container-notification-frame")));
 			WebElement closeButton = wait
